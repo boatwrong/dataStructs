131d3359dbf562a6f97c630e6fe911b32994187c
boatwrong <bryantboatright@ou.edu>
2022-01-14 08:05:12 -0600

    start assignment 1

---
+++ assignment1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int numGames;
+	int game[numGames];
+
+	cin >> numGames;
+
+	for(int i = 0; i < numGames; i++)
+	{
+		cin >> game[i];
+	}
+
+
+	return 0;
+}
