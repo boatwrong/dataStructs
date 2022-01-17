eb3ff40506c068bb12add9cc902cca827c5c6ad8
boatwrong <bryantboatright@ou.edu>
2022-01-16 22:51:40 -0600

    start assignment 3

---
+++ 3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct node 
+{
+    int value;
+    struct node *next;
+};
+
+// struct node *head = NULL;
+// struct node *current = NULL;
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
+
