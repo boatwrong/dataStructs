7b11e8188c59c3c6086e5fcabd47e925e2de59b2
boatwrong <bryantboatright@ou.edu>
2022-01-25 16:38:32 -0600

    Start assignment 4

---
+++ 4.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+struct node {
+    int value;
+    struct node *next;
+};
+
+struct node *head = NULL;
+
+void push(int x)
+{
+}
+
+void pop(int x)
+{
+}
+
+void sum()
+{
+}
+
+void query()
+{
+}
+
+int main(int argc, char *argv[])
+{
+    int numQueries;
+    scanf("%d", &numQueries);
+
+    for(int i=0; i < numQueries; i++)
+    {
+        query();
+    }
+    return 0;
+}
+
