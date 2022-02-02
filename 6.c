504100af72572226a3088a99a3f0194078477c8c
Bryant Boatright <bryantboatright@icloud.com>
2022-02-02 09:00:43 -0600

    new first draft, need to fnish operations and logic to pick ops basic structure done, not sure if struct still needed

--- 6.c
+++ 6.c
@@ -2,22 +2,51 @@
 #include<stdlib.h>
 #include<string.h>
 
-#define MAX_WORD_SIZE 100
+#define MAX_WORD_SIZE 15
 
 typedef struct {
     char* word[MAX_WORD_SIZE];
-
-}person ;
+}person;
 
 person tmp;
 
-int hash(char* word)
+int hash(char *word)
 {
-    return 0;
+    int sum;
+    for(int i=0; i<strlen(word)-1; i++)
+    {
+        sum += word[i];
+    }
+    return sum;
+}
+
+void takeInput(char *tmp)
+{
+    printf("taking input\n");
+    fgets(tmp, MAX_WORD_SIZE, stdin);
+    printf("input taken\n");
 }
 
 int main(int argc, char *argv[])
 {
+    int listSize;
+    int numTests[listSize];
+    printf("input number of tests\n");
+    scanf("%d", &listSize);
+    char T[MAX_WORD_SIZE];
+    char S[MAX_WORD_SIZE];
+    for(int i=0; i<listSize; i++)
+    {
+        numTests[i] = 0;
+        printf("test %d\n", i+1);
+        takeInput(T);
+        takeInput(S);
+        while(hash(T) != hash(S))
+        {
+            numTests[i]++;
+            // do operations...
+        }
+    }
     return 0;
 }
 
